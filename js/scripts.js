6442ac478ed3dc53c6b46adb823a94977aa1fd0c
Abdulrahman Mohamed <maanoatu@gmail.com>
2018-03-07 12:22:47 +0300

    added section 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
+var a = 0;
+
 //User Interface
 $(document).ready(function() {
-	$(window).scroll(function() {
-  	if($(document).scrollTop() > 500) {
-    	$('#nav').addClass('shrink');
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 500) {
+      $('#nav').addClass('shrink');
+    } else {
+      $('#nav').removeClass('shrink');
     }
-    else {
-    $('#nav').removeClass('shrink');
+
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $('.counter-value').each(function() {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+            countNum: countTo
+          },
+
+          {
+            duration: 700000,
+            easing: 'swing',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+              //alert('finished');
+            }
+          });
+      });
+      a = 1;
     }
+
   });
 });
